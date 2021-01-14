6f1ddbe628fbbc85060f5d910ae30a366ae3d466
collinradichel <radichel.collin@gmail.com>
2021-01-14 13:08:44 -0600

    built out the ShoppingList component (header and 2 buttons RESET and CLEAR). Wrote the GET route on the server-side to /shopping

--- server/routes/list.router.js
+++ server/routes/list.router.js
@@ -5,4 +5,23 @@ const pool = require('../modules/pool.js');
 // TODO - Add routes here...
 
 
+//GET route to get all the items from the database
+router.get('/', (req,res)=> {
+
+    //order by item
+    const queryText = `SELECT * FROM "shopping"
+                        ORDER BY "item";`
+    
+    pool.query(queryText)
+    .then(result => {
+        console.log(`Got shopping items back from the database:`, result);
+        // just send it! result.rows
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log(`Error in GET route server-side trying to make query ${queryText} `, error);
+        res.sendStatus(500);
+    })
+})
+
 module.exports = router;
