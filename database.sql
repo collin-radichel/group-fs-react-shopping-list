64eee72c25b53478675e6b84efa0795cf97035f1
collinradichel <radichel.collin@gmail.com>
2021-01-14 10:11:18 -0600

    added database.sql code

--- database.sql
+++ database.sql
@@ -1,2 +1,12 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE shopping (
+	id SERIAL PRIMARY KEY,
+	item VARCHAR(25) NOT NULL,
+	quantity INT NOT NULL,
+	unit VARCHAR(10) NOT NULL
+);
+
+INSERT INTO shopping (item, quantity, unit)
+VALUES ('Apples', 5 , 'lbs');
